b7fc69be259750781f9feda4bc92fed11cc4b827
bindi-v <bindi.vaghela@gmail.com>
2022-03-07 11:07:10 -0500

    added get-your-goal project generated from spring Initializer

---
+++ get-your-goal/src/main/java/com/company/getyourgoal/GetYourGoalApplication.java
@@ -0,0 +1,13 @@
+package com.company.getyourgoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetYourGoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetYourGoalApplication.class, args);
+	}
+
+}
