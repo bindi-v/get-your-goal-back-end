638e07bdf460b73a0073a013ad82c7532d1eb810
bindi-v <bindi.vaghela@gmail.com>
2022-03-10 11:59:30 -0500

    created random-goal-service App for microservice, service registry

---
+++ random-goal-service/random-goal-service/src/test/java/com/company/randomgoalservice/RandomGoalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.randomgoalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomGoalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
