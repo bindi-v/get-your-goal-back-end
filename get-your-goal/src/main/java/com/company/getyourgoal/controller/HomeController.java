a5a651417be8564957e78686b1f4ec862ee9e1c4
bindi-v <bindi.vaghela@gmail.com>
2022-03-10 11:35:43 -0500

    created Eureks-service-registry, and it is working well

--- get-your-goal/src/main/java/com/company/getyourgoal/controller/HomeController.java
+++ get-your-goal/src/main/java/com/company/getyourgoal/controller/HomeController.java
@@ -1,14 +1,18 @@
 package com.company.getyourgoal.controller;
 
+import com.company.getyourgoal.model.User;
 import com.company.getyourgoal.repository.CommentRepository;
 import com.company.getyourgoal.repository.GoalRepository;
 import com.company.getyourgoal.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 //@CrossOrigin("http://localhost:3000")
 @RestController
@@ -33,10 +37,13 @@ public class HomeController {
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model, HttpServletRequest request) {
+        if (request.getSession(false) != null) {
+            return "redirect:/";
+        }
+        model.addAttribute("user", new User());
         return "login";
     }
-
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/users/logout")
